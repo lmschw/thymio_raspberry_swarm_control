14365ebd18c9fed09cbb1d879734369531ea7204
Lilly <lillyschwarzenbach@gmail.com>
2026-07-06 14:28:44 +0200

    add blink experiment

---
+++ experiments/blink.py
@@ -0,0 +1,52 @@
+import asyncio
+
+class BlinkExperiment:
+
+    name = "blink"
+
+    def __init__(self, robot, config=None, logger=None):
+        self.robot = robot
+        self.config = config or {}
+        self.logger = logger
+
+        self.running = True
+        self.paused = False
+
+    async def run(self):
+        while self.running:
+
+            if self.paused:
+                await self.robot.stop()
+                await asyncio.sleep(0.1)
+                continue
+
+            await self.robot.top_led(0, 255, 0)
+
+            if self.logger:
+                self.logger.log(
+                    state={"led": "green"},
+                    command={}
+                )
+
+            await asyncio.sleep(1)
+
+            await self.robot.top_led(255, 0, 0)
+
+            if self.logger:
+                self.logger.log(
+                    state={"led": "red"},
+                    command={}
+                )
+
+            await asyncio.sleep(1)
+
+        await self.robot.top_led(0, 0, 0)
+
+    async def pause(self):
+        self.paused = True
+
+    async def resume(self):
+        self.paused = False
+
+    async def stop(self):
+        self.running = False
